5a9fba8eda5fd483c139dfd637ad5e57fc0e9418
Finn S <finnstainton@gmail.com>
2020-04-28 15:23:05 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-//const hostname = '127.0.0.1';
-//const port = 3000;
+/const hostname = 'https://hello-world-nodejs-122020.herokuapp.com';
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-//server.listen(port, hostname, () => {
-//  console.log(`Server running at http://${hostname}:${port}/`);
-//});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
