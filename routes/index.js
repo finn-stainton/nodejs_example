6d6f75ba563b94e637a9b8a9a7285e363caaacdb
Finn S <finnstainton@gmail.com>
2020-04-28 17:16:58 +1200

    Express Demo

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
-var getData = function () {
-    var data = {
-        'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
-        'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
-        'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg'
-    }
-    return data;
-}
+var express = require('express');
+var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.render('index', { title: 'Express', "data": getData() });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+module.exports = router;
